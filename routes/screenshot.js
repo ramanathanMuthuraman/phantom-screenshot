5d12d234428883170f1220f7501ef90ba663221d
ramanathan <ramanathan.gksvv@gmail.com>
2014-08-19 13:31:01 +0000

    screenshot service

--- routes/screenshot.js
+++ routes/screenshot.js
@@ -13,3 +13,32 @@ var url_parts = url.parse(req.url, true);
 });
 
 module.exports = router;
+
+
+/*
+ * 
+ * var express = require('express');
+var url = require('url');
+var webshot = require('webshot');
+var fs      = require('fs');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    
+  //  webshot(query.url,  function(err,renderStream) {
+          webshot('google.com',  function(err,renderStream) {
+       renderStream.on('data', function(data) {
+             res.writeHead(200, {'Content-Type': 'image/png'});
+   res.end(data);
+  });
+        
+    
+});
+ 
+});
+
+module.exports = router;
+
+ * /
